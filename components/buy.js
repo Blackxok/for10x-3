fe5fb483455839ba710dccc23e24485b722b5f67
Blackxok <nasridoni1@gmail.com>
2023-05-24 23:54:14 +0500

    cng 2405

---
+++ components/buy.js
@@ -0,0 +1,11 @@
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 5,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
